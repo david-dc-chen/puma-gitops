70e1768ba02eda1adc440479400b911de70323f1
david-dc.chen@broadcom.com <david-dc.chen@broadcom.com>
2019-05-22 12:57:24 -0400

    updated script and secrets

--- scripts/makeImageCreds.sh
+++ scripts/makeImageCreds.sh
@@ -17,7 +17,7 @@ done
 OUT="$(mktemp)"
 echo "gateway:" >> "$OUT"
 echo "  imageCredentials:" >> "$OUT"
-echo "    password: \"$password\"" >> "$OUT"
+echo "      password: \"$password\"" >> "$OUT"
 kubectl create secret generic image-creds --dry-run  -n "$namespace"  -o yaml --from-file=image-creds.yaml="$OUT" | kubeseal --format yaml > "$destination"
 if [ $? -eq 0 ]; then
     echo "Secret created in $destination"
